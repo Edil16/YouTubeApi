218120dd52a183042b8133a31860fe49c267d5eb
Edil16 <edila460@gmail.com>
2022-10-18 15:54:43 +0600

    first commit

---
+++ app/src/main/java/kg/example/youtubeapi/core/ui/BaseViewModel.kt
@@ -0,0 +1,9 @@
+package kg.example.youtubeapi.core.ui
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel: ViewModel() {
+
+    val loading = MutableLiveData<Boolean>()
+}
